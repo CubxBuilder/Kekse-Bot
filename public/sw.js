905b6d83e29afb4d2a4cbd662358e36f4075188d
CubeBuilder <CubeBuilder@web.de>
2026-05-17 01:22:19 +0200

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'Kekse-Bot';
+const FILES = ['/', '/index.html', '/manifest.json', '/dashboard.html', '/dashboard-manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
